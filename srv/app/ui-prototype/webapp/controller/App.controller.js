f38f6a3564c04c18b57f70a3f15fe0c9fd615c15
Dozo <64401443+staub03@users.noreply.github.com>
2023-01-27 21:01:28 +0100

    Merged and important adjustments

---
+++ srv/app/ui-prototype/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+  ],
+  function (BaseController, MessageToast) {
+    "use strict";
+
+    return BaseController.extend("ns.uiprototype.controller.App", {
+      onInit() {
+      }
+    })
+  }
+);
---
+++ srv/app/ui-prototype/webapp/model/formatter.js
@@ -0,0 +1,12 @@
+sap.ui.define([], function () {
+	"use strict";
+
+	return {
+        division100: function (value1, value2) {
+            return value1/value2 * 100;
+        },
+        multiplication: function (value1, value2) {
+            return value1*value2;
+        }
+	};
+});
