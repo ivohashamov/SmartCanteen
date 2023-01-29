1f280add15fe3391df405a8cc5d6cae6ea7f075d
Christian Heisig <c.heisig@outlook.de>
2023-01-29 21:57:22 +0100

    Few features solved

--- app/ui-prototype/webapp/controller/Cards.controller.js
+++ app/ui-prototype/webapp/controller/Cards.controller.js
@@ -4,16 +4,22 @@ sap.ui.define([
     "../model/formatter"
  ], function (Controller, JSONModel, formatter) {
     "use strict";
-
+    var hi = 0
     return Controller.extend("ns.uiprototype.controller.Cards", {
         formatter: formatter,
         onInit: async function () {
-            //SAP UI5 cards
-            var cardManifests = new JSONModel();
-            cardManifests.loadData("model/cardManifests.json");
-            console.log(cardManifests);
-            this.getView().setModel(cardManifests, "manifests");
+            //Load default JSONModel
+            await fetch('./model/defaultData.json').then(response => response.json()).then(data => $.sap.defaultData = new JSONModel(data))
 
+            //Firstly update the cards
+            this.updateFioriCards()
+            this.updateAnalyticsCards()
+
+            //Set time interval for updating the cards
+            this._trigger = new sap.ui.core.IntervalTrigger(3 * 1000 /* refresh every 3sec */)
+            this._trigger.addListener(this.onRefreshTriggered, this);
+        },
+        updateFioriCards: async function() {
             //Fiori cards
             var oQueueOccupanciesModel = new JSONModel();  
             var query = jQuery.ajax({
@@ -21,10 +27,14 @@ sap.ui.define([
                 contentType: "application/json",
                 url:"http://localhost:4004/API_front/queueLengths?$orderby=date desc",
                 success: function(data, status) {
-                    oQueueOccupanciesModel.setData(data["value"][0])
+                    if(data["value"].length > 0)
+                        oQueueOccupanciesModel.setData(data["value"][0])
+                    else
+                        oQueueOccupanciesModel.setData($.sap.defaultData.getProperty("/QueueOccupancies"))
                 },
                 fail: function(data, status) {
                     //todo
+                    console.log("hey")
                 }
             })
             this.getView().setModel(oQueueOccupanciesModel, "QueueOccupancies")
@@ -36,7 +46,10 @@ sap.ui.define([
                 contentType: "application/json",
                 url:"http://localhost:4004/API_front/canteenOccupancies?$orderby=date desc",
                 success: function(data, status) {
-                    oOccupiedTablesModel.setData(data["value"][0])
+                    if(data["value"].length > 0)
+                        oOccupiedTablesModel.setData(data["value"][0])
+                    else
+                        oOccupiedTablesModel.setData($.sap.defaultData.getProperty("/OccupiedTables"))
                 }
             })
             this.getView().setModel(oOccupiedTablesModel, "OccupiedTables")
@@ -56,6 +69,15 @@ sap.ui.define([
             //Analyctical card
             var oVizFrame = this.oVizFrame = this.getView().byId("analyticalCard");
             //oVizFrame
+        },
+        updateAnalyticsCards: async function()  {
+            //SAP UI5 cards
+            var cardManifests = new JSONModel();
+            cardManifests.loadData("model/cardManifests.json");
+            this.getView().setModel(cardManifests, "manifests");
+        },
+        onRefreshTriggered: async function () {
+            await this.updateFioriCards()
         }
     });
  });
