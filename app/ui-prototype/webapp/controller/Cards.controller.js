be6f35f39301fc37551501ee04fc342c2135b1c6
Christian Heisig <c.heisig@outlook.de>
2023-02-09 22:24:18 +0100

    Adjusted persitent settings saving

--- app/ui-prototype/webapp/controller/Cards.controller.js
+++ app/ui-prototype/webapp/controller/Cards.controller.js
@@ -1,15 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter",
-    'sap/viz/ui5/data/FlattenedDataset',
-    'sap/viz/ui5/controls/common/feeds/FeedItem',
-    'sap/viz/ui5/controls/Popover',
-    'sap/viz/ui5/controls/VizFrame',
-    'sap/viz/ui5/format/ChartFormatter',
-    'sap/viz/ui5/api/env/Format',
-    'sap/ui/core/Title'
- ], function (Controller, JSONModel, formatter, FlattenedDataset, FeedItem, Popover, VizFrame, ChartFormatter, Format, Title) {
+    "../model/formatter"
+ ], function (Controller, JSONModel, formatter) {
     "use strict";
     var currentWeekday = 0
     const weekdays = {
@@ -28,14 +21,14 @@ sap.ui.define([
             await fetch('./model/defaultData.json').then(response => response.json()).then(data => $.sap.defaultData = new JSONModel(data))
 
             //Firstly update the cards
-            this.updateFioriCards()
-            this.updateAnalyticsCards()
+            this.updateFioriCardModels()
+            this.setAnalyticsCards()
 
             //Set time interval for updating the cards
             this._trigger = new sap.ui.core.IntervalTrigger(6 * 1000 /* refresh every x sec */)
             this._trigger.addListener(this.onRefreshTriggered, this);
         },
-        updateFioriCards: async function() {
+        updateFioriCardModels: async function() {
             //Fiori cards
             var oQueueOccupanciesModel = new JSONModel();  
             var query = jQuery.ajax({
@@ -49,8 +42,7 @@ sap.ui.define([
                         oQueueOccupanciesModel.setData($.sap.defaultData.getProperty("/QueueOccupancies"))
                 },
                 fail: function(data, status) {
-                    //todo
-                    console.log("hey")
+                    console.log(status)
                 }
             })
             this.getView().setModel(oQueueOccupanciesModel, "QueueOccupancies")
@@ -82,13 +74,25 @@ sap.ui.define([
             })
             this.getView().setModel(oTotalTablesModel, "TotalTables")
 
-            //Analyctical card
-            var oVizFrame = this.oVizFrame = this.getView().byId("analyticalCard");
-            //oVizFrame
-        },
-        updateAnalyticsCards: async function()  {
             //Current Analytics
-            this.updateCurrentAnalyticsCard()
+            var oAnalyticsModel = new JSONModel();
+            var query2 = jQuery.ajax({
+                type: "GET", 
+                contentType: "application/json",
+                url:"http://localhost:4004/API_front/canteenOccupancies?$apply=filter(entity_ID eq 1 and day(date) eq " + new Date().getDate() + ")/groupby((hour),aggregate(count with average as averageCount))",
+                success: function(data, status) {
+                    if(data["value"].length > 0) {
+                        for (let i = 0; i< data["value"].length; i++) {
+                            data["value"][i]["averageCount"] = Math.floor(data["value"][i]["averageCount"])
+                        }
+                        oAnalyticsModel.setData(data)
+                    }   
+                }
+            })
+            this.getView().setModel(oAnalyticsModel, "a1")
+        },
+        setAnalyticsCards: async function()  {
+            //Current Analytics
             var vizFrame = this.getView().byId("idVizFrame")
             vizFrame.setVizProperties({
                 plotArea: {
@@ -119,7 +123,7 @@ sap.ui.define([
             //Past Analytics
             var currentDate = new Date()
             currentWeekday = currentDate.getDay()
-            this.updatePastAnalyticsCard(Object.keys(weekdays).find(key => weekdays[key] === currentWeekday))
+            this.updatePastAnalyticsModel(Object.keys(weekdays).find(key => weekdays[key] === currentWeekday))
             var vizFrame = this.getView().byId("vizFrameWeekly")
             vizFrame.setVizProperties({
                 plotArea: {
@@ -147,24 +151,21 @@ sap.ui.define([
                 }
             })
         },
-        updateCurrentAnalyticsCard: async function() {
-            var oOccupiedTablesModel = new JSONModel();
-            var query2 = jQuery.ajax({
-                type: "GET", 
-                contentType: "application/json",
-                url:"http://localhost:4004/API_front/canteenOccupancies?$apply=filter(entity_ID eq 1 and day(date) eq " + new Date().getDate() + ")/groupby((hour),aggregate(count with average as averageCount))",
-                success: function(data, status) {
-                    if(data["value"].length > 0) {
-                        for (let i = 0; i< data["value"].length; i++) {
-                            data["value"][i]["averageCount"] = Math.floor(data["value"][i]["averageCount"])
-                        }
-                        oOccupiedTablesModel.setData(data)
-                    }   
-                }
-            })
-            this.getView().setModel(oOccupiedTablesModel, "a1")
+        onBeforePress: function () {
+            if(currentWeekday > 1) {
+                currentWeekday = currentWeekday - 1
+                var wDay = Object.keys(weekdays).find(key => weekdays[key] === currentWeekday)
+                this.updatePastAnalyticsModel(wDay)
+            }
         },
-        updatePastAnalyticsCard: async function(weekday)
+        onNextPress: function () {
+            if(currentWeekday < 5) {
+                currentWeekday = currentWeekday + 1
+                var wDay = Object.keys(weekdays).find(key => weekdays[key] === currentWeekday)
+                this.updatePastAnalyticsModel(wDay)
+            }
+        },
+        updatePastAnalyticsModel: async function(weekday)
         {
             this.getView().setModel(new JSONModel({'day': weekday}), "weekday");
             var oOccupiedTablesModel = new JSONModel();
@@ -184,22 +185,8 @@ sap.ui.define([
             })
             this.getView().setModel(oOccupiedTablesModel, "a2")
         },
-        onBeforePress: function () {
-            if(currentWeekday > 1) {
-                currentWeekday = currentWeekday - 1
-                var wDay = Object.keys(weekdays).find(key => weekdays[key] === currentWeekday)
-                this.updatePastAnalyticsCard(wDay)
-            }
-        },
-        onNextPress: function () {
-            if(currentWeekday < 5) {
-                currentWeekday = currentWeekday + 1
-                var wDay = Object.keys(weekdays).find(key => weekdays[key] === currentWeekday)
-                this.updatePastAnalyticsCard(wDay)
-            }
-        },
         onRefreshTriggered: async function () {
-            await this.updateFioriCards()
+            await this.updateFioriCardModels()
         },
     });
  });
--- app/ui-prototype/webapp/controller/Dashboard.controller.js
+++ app/ui-prototype/webapp/controller/Dashboard.controller.js
@@ -1,26 +1,25 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast",
   "sap/ui/model/json/JSONModel",
-  "../model/formatter",
-  'sap/viz/ui5/data/FlattenedDataset',
-  'sap/viz/ui5/controls/common/feeds/FeedItem',
-  'sap/viz/ui5/controls/Popover',
-  'sap/viz/ui5/controls/VizFrame',
-  'sap/viz/ui5/format/ChartFormatter',
-  'sap/viz/ui5/api/env/Format',
-  'sap/ui/core/Title'
+  "../model/formatter"
 ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, MessageToast, JSONModel, formatter, FlattenedDataset, FeedItem, Popover, VizFrame, ChartFormatter, Format, Title) {
+  function (Controller, JSONModel, formatter) {
     "use strict";
     return Controller.extend("ns.uiprototype.controller.Dashboard", {
       formatter: formatter,
       onInit: function () {
       },
+      onAfterRendering: function () {
+        //retrieve settingsModel
+        var oStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+        var settingsModel = oStore.get("settingsModel");
+        this.getView().setModel(new JSONModel(settingsModel), "settingsModel")
 
+        //update settings
+      },
       onItemSelect: function (oEvent) {
         var oItem = oEvent.getParameter("item");
         this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
@@ -45,41 +44,9 @@ sap.ui.define([
         //get the settings model
         var settingsModel = this.getView().getModel("settingsModel")
 
-        //update the settings model
-        var sCheckBoxId = oEvent.getSource().getId();
-        switch (sCheckBoxId) {
-          case "container-ns.uiprototype---dashboard--myCheckBox":
-            var oCheckBox = this.getView().byId("myCheckBox");
-            settingsModel.setProperty("/card1", oCheckBox.getProperty("selected"));
-            break;
-          case "container-ns.uiprototype---dashboard--myCheckBox1":
-            var oCheckBox = this.getView().byId("myCheckBox1");
-            settingsModel.setProperty("/card2", oCheckBox.getProperty("selected"));
-            break;
-          case "container-ns.uiprototype---dashboard--myCheckBox2":
-            var oCheckBox = this.getView().byId("myCheckBox2");
-            settingsModel.setProperty("/card3", oCheckBox.getProperty("selected"));
-            break;
-          case "container-ns.uiprototype---dashboard--myCheckBox3":
-            var oCheckBox = this.getView().byId("myCheckBox3");
-            settingsModel.setProperty("/card4", oCheckBox.getProperty("selected"));
-            break;
-          case "container-ns.uiprototype---dashboard--myCheckBox4":
-            var oCheckBox = this.getView().byId("myCheckBox4");
-            settingsModel.setProperty("/card5", oCheckBox.getProperty("selected"));
-            break;
-          case "container-ns.uiprototype---dashboard--myCheckBox5":
-            var oCheckBox = this.getView().byId("myCheckBox5");
-            settingsModel.setProperty("/card6", oCheckBox.getProperty("selected"));
-            break;
-          case "container-ns.uiprototype---dashboard--myCheckBox6":
-            var oCheckBox = this.getView().byId("myCheckBox6");
-            settingsModel.setProperty("/card7", oCheckBox.getProperty("selected"));
-            break;
-          }
-
-          //set the settings model
-          this.getView().setModel(settingsModel, "settingsModel")
+        //and safe it persistant
+        var oStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+        oStore.put("settingsModel", settingsModel.getProperty("/"));
       },
     });
   });
